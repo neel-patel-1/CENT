955af4f6a6f2fa13814e7b264bc9632f9bfea3aa
Your Name <you@example.com>
2026-02-08 00:32:47 -0500

    start RMSNorm

--- cent_simulation/kernel_sim.py
+++ cent_simulation/kernel_sim.py
@@ -8,4 +8,22 @@ import subprocess
 
 if __name__ == "__main__":
   # GemV
-  gemv()
+  # gemv()
+
+  # RMSNorm
+
+  # partially functional only
+  # generates traces for theh Dot Products on AiM
+  # but uses a python-executed loop to aggregate the
+  # sum of squares of input elements
+
+  args = get_args()
+  from kernels.dic_model import create_dic_model
+  dic_model = create_dic_model(args)
+
+  TB = TransformerBlockLlama(dic_model, args)
+  TB.memory_mapping()
+  TB.memory_mapping_verification()
+
+  x_pow_sum = 0
+  op_size = (TB.dic_shape["x_neighbor_bank"][0] - 1)
--- cent_simulation/kernels/llama_kernels.py
+++ cent_simulation/kernels/llama_kernels.py
@@ -1,2 +1,14 @@
 from Llama import TransformerBlockLlama
 from model_config import create_dic_model
+import os
+from utils import get_args
+
+def RMSNorm():
+  args = get_args()
+  dic_model = create_dic_model(args)
+
+  kernel_name = "RMSNorm_{D}"
+  outdir = f"./traces/"
+  os.makedirs(outdir, exist_ok=True)
+  # Create input vector
+  D=512
